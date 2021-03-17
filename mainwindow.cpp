f0b3c1da19e765630a726a23ee31317dfcb1d8bd
Maximover <72668610+Maximover@users.noreply.github.com>
2021-03-17 14:13:14 +0100

    Poprawki

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,32 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    showFullScreen();
+
+    tlo = new Tlo();
+    scene = new QGraphicsScene();
+    theme = new QMediaPlayer();
+
+    scene->addItem(tlo); //dodanie tła do sceny
+    ui->graphicsView->setScene(scene); //ustawienie sceny
+
+    theme->setMedia(QUrl(""));//menu theme
+    theme->setVolume(100);
+    //theme->play();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::keyPressEvent(QKeyEvent *event) //zczytanie z klawiatury
+{
+    if(event->key()==Qt::Key_Q){
+        QApplication::quit();
+    }
+
+}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QtCore>
+#include <QString>
+#include <QPixmap>
+#include <QKeyEvent>
+#include <QMediaPlayer>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QRandomGenerator>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include "tlo.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +26,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+protected:
+    void keyPressEvent(QKeyEvent *event);
 
 private:
     Ui::MainWindow *ui;
+    Tlo *tlo;
+    QGraphicsScene *scene;
+    QMediaPlayer *theme;
+
+
 };
 #endif // MAINWINDOW_H
